5ab7c48989c4a914a22ed63694c440ec54a63b76
Gabriel Cruzato <gabrielcruzato@gmail.com>
2025-05-22 23:49:46 -0300

    MVVM Skeleton for Chat Messenger app

---
+++ Chatapp/Chatapp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using ChatClient.MVVM.Core;
+using ChatClient.Networking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient.MVVM.ViewModel
+{
+    public class MainViewModel
+    {
+        public required RelayCommand ConnectToServerCommand { get; set; }
+
+        private Server? _server;
+
+        public MainViewModel()
+        {
+        }
+    }
+}
---
+++ Chatapp/Chatapp/Networking/Server.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient.Networking
+{
+
+     public class Server
+    {
+        TcpClient client;
+
+        public Server(string ip, int port)
+        {
+            client = new TcpClient(ip, port);
+        }
+
+        public void ConnectToServer()
+        {
+            if (!client.Connected)
+            {   
+                client.Connect("127.0.0.1", 7891);
+                Console.WriteLine("Connected to server");
+            }
+            else
+            {
+                Console.WriteLine("Failed to connect to server");
+            }
+        }
+    }
+}
